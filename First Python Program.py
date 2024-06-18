4e5db322529a179693e29792c426347c2c02c25a
grantebailey25 <150882070+grantebailey25@users.noreply.github.com>
2024-06-17 23:58:43 -0400

    Adding my first practice program

---
+++ First Python Program.py
@@ -0,0 +1,17 @@
+# Have the program print Hello World and a saying that you like
+print ('Hello World! You can do anything if you put your mind to it.')
+
+# Program prompts the user for their name and saves that input as a variable
+userInput = input('Type some input: ')
+
+# Print the user's inputed text
+print ('You typed: ' + userInput)
+
+# Print results of arithmetic
+print (1 + 2 + 3 + 4)
+
+# Print a longer saying
+print ('For God so loved the world that he gave his one and only son.\nSo that whoever believes in him shall not perish, but have eternal life.')
+
+# Tell the user goodbye in a different language
+print ("Adios y buena suerte en tu adventura con Python.")
