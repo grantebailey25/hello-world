bf7a802fc05bf286a8ec304b8ab7d4b1291bfe1e
grantebailey25 <150882070+grantebailey25@users.noreply.github.com>
2024-07-21 14:04:09 -0400

    Update Calendar Program.py
    Separating my code into funcitons

--- Calendar Program.py
+++ Calendar Program.py
@@ -1,35 +1,43 @@
+def printHeader():
+    print('  Sun    Mon    Tue    Wed    Thu    Fri    Sat')
+    for _ in range (0, 7):
+        print('+' + ('-' * 6), end = '')
+    print('+')
+def firstWeek(dayCount):
+    numBlanks = (8 - firstSunday) % 7
+    for _ in range(0, numBlanks):
+        print('|      ', end = '')
+    for i in range(1, 8 - numBlanks):
+        stringI = str(i)
+        print('|  ' + stringI + '   ', end = '')
+        dayCount = dayCount + 1
+    print('|')
+    return dayCount
+def restOfMonth(dayCount, spaces):
+    while dayCount <= numDays:
+        for _ in range (7):
+            if dayCount > numDays:
+                print('|      ', end = '')
+            else:
+                if dayCount >= 10:
+                    spaces = 2
+                stringDayCount = str(dayCount)
+                print('|  ' + stringDayCount + (' ' * spaces), end = '')
+                dayCount += 1
+        print('|')
+def printBottomBorder():
+    for _ in range (7):
+        print('+' + ('-' * 6), end = '')
+    print('+')
+    
 print('How many days are in the month?', end = ' ')
 numDays = int(input())
 print('What day will be the first Sunday of the month?', end = ' ')
 firstSunday = int(input())
-print('  Sun    Mon    Tue    Wed    Thu    Fri    Sat')
-for i in range (0, 7):
-    print('+' + ('-' * 6), end = '')
-print('+')
+printHeader()
 dayCount = 0
-numBlanks = (8 - firstSunday) % 7
-for i in range(0, numBlanks):
-    print('|      ', end = '')
-for i in range(1, 8 - numBlanks):
-    stringI = str(i)
-    print('|  ' + stringI + '   ', end = '')
-    dayCount += 1
-print('|')
+dayCount = firstWeek(dayCount)
 spaces = 3
 dayCount += 1
-while dayCount <= numDays:
-    for i in range (7):
-        if dayCount > numDays:
-            print('|      ', end = '')
-        else:
-            if dayCount >= 10:
-                spaces = 2
-            stringDayCount = str(dayCount)
-            print('|  ' + stringDayCount + (' ' * spaces), end = '')
-            dayCount += 1
-    print('|')
-for i in range (0, 7):
-    print('+' + ('-' * 6), end = '')
-print('+')
-
-#After you complete this program, try to make it more efficient using lists
+restOfMonth(dayCount, spaces)
+printBottomBorder()
